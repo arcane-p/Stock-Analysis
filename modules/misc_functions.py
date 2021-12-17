ccaaea154d45e3af607dcf1f51eb8021806ac175
TheArcanePowers <leoman.coppi@gmail.com>
2021-12-17 06:15:37 +0000

    Made logging to use application-wide package

--- modules/misc_functions.py
+++ modules/misc_functions.py
@@ -1,40 +1,39 @@
 import urllib.request as request
 from contextlib import closing
 import re
-import logging
+from pretty_print import PL
 
-log = logging.getLogger(__name__)
-
-log.debug("Doing something!")
 
 def load_tickers():
-    log.info("Downloading ticker validation...")
+    PL.print("Downloading ticker validation...", 1)
     valid_tickers = []
-    log.debug("Requesting from NASDAQ")
+    PL.print("Requesting from NASDAQ", 0)
     with closing(request.urlopen("ftp://ftp.nasdaqtrader.com/SymbolDirectory/nasdaqlisted.txt")) as r:
         for line in r:
             line = line.decode()
             newline = (re.search('[^|]*', line).group(),)
             valid_tickers += (newline)
-    log.debug("Done with NASDAQ")
-    log.debug("Requesting from Other Listed")
+    PL.print("Done with NASDAQ", 0)
+    PL.print("Requesting from Other Listed", 0)
     with closing(request.urlopen("ftp://ftp.nasdaqtrader.com/SymbolDirectory/otherlisted.txt")) as r:
         for line in r:
             line = line.decode()
             newline = (re.search('[^|]*', line).group(),)
             valid_tickers += (newline)
-    log.debug("Done with Other Listed")
-    log.info("Finished downloading ticker validation!")
+    PL.print("Done with Other Listed", 0)
+    PL.print("Finished downloading ticker validation!", 1)
     return valid_tickers
 
+
 def get_input(input_message: str, type="str"):
-    log.debug(f"Getting input about {input_message}")
+    PL.print(f"Getting input about {input_message}", 0)
     # Type can be Bool, str, int
     input_message = str(input_message)
     type = str(type).lower()
     if type == "bool":
         possible_answers = " (Y/yes/N/no)"
     elif type != "str" and type != "int":
+        PL.print("get_input type is incorrect!", 4)
         raise Exception("Type must be bool, str, or int")
     else:
         possible_answers = ""
@@ -51,16 +50,19 @@ def get_input(input_message: str, type="str"):
                 elif answer == "N":
                     return False
             except ValueError:
-                print("Input error! Please try again.\n")
+                PL.print("Input error! Please try again.", 3)
+                print()  # newline
 
         if type == "str":
             try:
                 return str(answer)
             except ValueError:
-                print("Input error! Please try again.\n")
+                PL.print("Input error! Please try again.", 3)
+                print()  # newline
 
         if type == "int":
             try:
                 return int(answer)
             except ValueError:
-                print("Input error! Please enter numerical values only.\n")
+                PL.print("Input error! Please enter numerical values only.", 3)
+                print()  # newline
